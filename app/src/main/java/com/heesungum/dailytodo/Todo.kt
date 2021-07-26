c48c115b26ff9509beefd00e767034354488de4f
HeeSungUm <50452867+HeeSungUm@users.noreply.github.com>
2021-07-26 17:43:22 +0900

    data class 생성

---
+++ app/src/main/java/com/heesungum/dailytodo/Todo.kt
@@ -0,0 +1,25 @@
+package com.heesungum.dailytodo
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Todo")
+data class Todo (
+    @PrimaryKey(autoGenerate = true)
+    var id: Long?,
+
+    @ColumnInfo(name = "title")
+    var title: String,
+
+    @ColumnInfo(name = "description")
+    var description: String,
+
+    @ColumnInfo(name = "createdDate")
+    var createdDate:Long,
+
+    @ColumnInfo(name = "times")
+    var times:Long
+){
+    constructor(): this(null, "", "", -1, 0)
+}
