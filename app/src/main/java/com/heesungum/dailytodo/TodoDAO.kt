b3dc933245dadff9d6a4ce5bdb7651e82c8bb541
HeeSungUm <50452867+HeeSungUm@users.noreply.github.com>
2021-07-26 21:06:36 +0900

    TodoDAO 생성

---
+++ app/src/main/java/com/heesungum/dailytodo/TodoDAO.kt
@@ -0,0 +1,16 @@
+package com.heesungum.dailytodo
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface TodoDAO {
+
+    @Query("select * from Todo order by createdDate ASC")
+    fun getTodoList(): LiveData<List<Todo>>
+
+    @Insert
+    fun insertTodo(todo: Todo)
+}
