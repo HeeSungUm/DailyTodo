332dbacdaa28ea0d3794c6051e614110f2630da1
HeeSungUm <50452867+HeeSungUm@users.noreply.github.com>
2021-07-26 13:35:36 +0900

    bottom navigation bar 코드 추가

--- app/src/main/java/com/heesungum/dailytodo/MainActivity.kt
+++ app/src/main/java/com/heesungum/dailytodo/MainActivity.kt
@@ -2,10 +2,44 @@ package com.heesungum.dailytodo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.heesungum.dailytodo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val fragmentHome by lazy { HomeFragment() }
+
+    private var mBinding: ActivityMainBinding? = null
+
+    private val binding get() = mBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.mainBottomNavigation.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.page_home -> {
+                    changeFragment(fragmentHome)
+                }
+            }
+            true
+        }
+
+
+    }
+
+    private fun changeFragment(fragment: Fragment){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frame_container, fragment)
+            .commit()
+    }
+
+    override fun onDestroy() {
+        mBinding = null
+        super.onDestroy()
     }
 }
