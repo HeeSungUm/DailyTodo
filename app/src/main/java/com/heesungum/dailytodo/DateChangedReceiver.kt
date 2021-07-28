28ffb04e0dfcec6f0c08d397293bf182bdfcd290
HeeSungUm <50452867+HeeSungUm@users.noreply.github.com>
2021-07-28 20:29:10 +0900

    DateChangedReceiver 추가

---
+++ app/src/main/java/com/heesungum/dailytodo/DateChangedReceiver.kt
@@ -0,0 +1,25 @@
+package com.heesungum.dailytodo
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import androidx.lifecycle.MutableLiveData
+import java.text.SimpleDateFormat
+import java.util.*
+
+class DateChangedReceiver: BroadcastReceiver() {
+    private val today: MutableLiveData<String> = MutableLiveData()
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val calender: Calendar = Calendar.getInstance()
+        val dateFormat = "yyyy/mm/dd"
+        today.value = SimpleDateFormat(dateFormat, Locale.KOREA).format(calender.time)
+    }
+    fun getDate(): MutableLiveData<String>{
+        val calender: Calendar = Calendar.getInstance()
+        val dateFormat = "yyyy/mm/dd"
+        today.value = SimpleDateFormat(dateFormat, Locale.KOREA).format(calender.time)
+        return today
+    }
+
+}
