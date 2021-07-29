42271b352cbfe2928cf4ec4b38f2b094de1fabb8
HeeSungUm <50452867+HeeSungUm@users.noreply.github.com>
2021-07-29 14:24:52 +0900

    HomeFragment recyclerview 코드 추가

--- app/src/main/java/com/heesungum/dailytodo/HomeFragment.kt
+++ app/src/main/java/com/heesungum/dailytodo/HomeFragment.kt
@@ -5,11 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.heesungum.dailytodo.databinding.FragmentMainBinding
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.heesungum.dailytodo.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-    private var mBinding: FragmentMainBinding? = null
+    private var viewModel: TodoViewModel = TodoViewModel(requireActivity().application)
+    private var mBinding: FragmentHomeBinding? = null
     private val binding get() = mBinding!!
 
     override fun onCreateView(
@@ -17,8 +19,9 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        mBinding = FragmentMainBinding.inflate(inflater, container, false)
+        mBinding = FragmentHomeBinding.inflate(inflater, container, false)
+        mBinding!!.homeRv.layoutManager = LinearLayoutManager(context)
+        mBinding!!.homeRv.adapter = context?.let { TodoRvAdapter(it, viewModel.getTodos()) }
         return binding.root
     }
 
