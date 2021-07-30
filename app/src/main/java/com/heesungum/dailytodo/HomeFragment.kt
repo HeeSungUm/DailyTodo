72fddac535cebfc8172f662b16b45d2523352ff9
HeeSungUm <50452867+HeeSungUm@users.noreply.github.com>
2021-07-30 21:59:03 +0900

    HomeFragment dataBinding, todo list observer implemented

--- app/src/main/java/com/heesungum/dailytodo/HomeFragment.kt
+++ app/src/main/java/com/heesungum/dailytodo/HomeFragment.kt
@@ -4,24 +4,37 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.heesungum.dailytodo.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-
-    private var viewModel: TodoViewModel = TodoViewModel(requireActivity().application)
     private var mBinding: FragmentHomeBinding? = null
     private val binding get() = mBinding!!
+    private lateinit var todoViewModel: TodoViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        mBinding = FragmentHomeBinding.inflate(inflater, container, false)
-        mBinding!!.homeRv.layoutManager = LinearLayoutManager(context)
-        mBinding!!.homeRv.adapter = context?.let { TodoRvAdapter(it, viewModel.getTodos()) }
+        val adapter = context?.let { TodoRvAdapter(it) }
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        activity?.let {
+            todoViewModel = ViewModelProvider(it).get(TodoViewModel::class.java)
+            mBinding!!.homeRv.layoutManager = LinearLayoutManager(context)
+            mBinding!!.homeRv.adapter = adapter
+            mBinding!!.lifecycleOwner = viewLifecycleOwner
+            mBinding!!.viewModel = todoViewModel
+            mBinding!!.activity = activity as MainActivity
+        }
+
+
+        todoViewModel.todos.observe(viewLifecycleOwner, {
+            adapter?.setData(it)
+        })
         return binding.root
     }
 
