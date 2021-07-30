f5c26279d23e213b2add9cffd401c133a63eae60
HeeSungUm <50452867+HeeSungUm@users.noreply.github.com>
2021-07-30 16:03:19 +0900

    Receiver- dateFormat, getToday method 삭제, init 추가

--- app/src/main/java/com/heesungum/dailytodo/DateChangedReceiver.kt
+++ app/src/main/java/com/heesungum/dailytodo/DateChangedReceiver.kt
@@ -3,23 +3,32 @@ package com.heesungum.dailytodo
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import androidx.databinding.ObservableField
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import java.text.SimpleDateFormat
 import java.util.*
 
 class DateChangedReceiver: BroadcastReceiver() {
-    private val today: MutableLiveData<String> = MutableLiveData()
+    private val _today: MutableLiveData<String> = MutableLiveData()
+    val today: LiveData<String>
+        get() = _today
+    private val dateFormat = "yy/MM/dd"
+
+    init {
+        val calender: Calendar = Calendar.getInstance()
+        var text = ""
+        text = SimpleDateFormat(dateFormat, Locale.KOREA).format(calender.time)
+        text = "$text 할 일"
+        _today.value = text
+    }
 
     override fun onReceive(context: Context?, intent: Intent?) {
         val calender: Calendar = Calendar.getInstance()
-        val dateFormat = "yyyy/mm/dd"
-        today.value = SimpleDateFormat(dateFormat, Locale.KOREA).format(calender.time)
-    }
-    fun getToday(): MutableLiveData<String>{
-        val calender: Calendar = Calendar.getInstance()
-        val dateFormat = "yyyy/mm/dd"
-        today.value = SimpleDateFormat(dateFormat, Locale.KOREA).format(calender.time)
-        return today
+        var text = ""
+        text = SimpleDateFormat(dateFormat, Locale.KOREA).format(calender.time)
+        text = "$text 할 일"
+        _today.value = text
     }
 
 }
